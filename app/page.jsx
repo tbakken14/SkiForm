0b7ecace54a0a83f1df97998ddf755cb0acd78b9
tbakken14 <tbakken14@gmail.com>
2025-03-11 01:02:02 -0700

    Fix formating

--- app/page.jsx
+++ app/page.jsx
@@ -7,8 +7,8 @@ In order to improve clinics, we'd like to hear from you!`;
 export default async function Page() {
     return (
         <>
-            <h1 className="justify-center">Ski Form</h1>
-            <Markdown content={explainer} className="justify-center"/>
+            <h1 className="flex w-full justify-center">Ski Form</h1>
+            <Markdown content={explainer} className="flex w-full justify-center"/>
             <div className="flex w-full pt-12 justify-center">
                 <FeedbackForm />
             </div>
