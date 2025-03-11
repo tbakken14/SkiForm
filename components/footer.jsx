80dce3502ee9734942d74e988d2eb02294efa128
tbakken14 <tbakken14@gmail.com>
2025-03-11 01:39:11 -0700

    Update logo

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,8 +3,7 @@ import Link from 'next/link';
 export function Footer() {
     return (
         <>
-            <div className='logo-s1'></div>
-            <div className='logo-s2'></div>
+            
         </>
     );
 };
