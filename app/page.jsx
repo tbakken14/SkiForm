63e5d7423c09386cae837fd125a5eb53855de4cc
tbakken14 <tbakken14@gmail.com>
2025-03-10 23:24:33 -0700

    Fixed previous update

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,14 @@ import { RandomQuote } from 'components/random-quote';
 import { Markdown } from 'components/markdown';
 import { ContextAlert } from 'components/context-alert';
 import { getNetlifyContext } from 'utils';
+import { FeedbackForm } from 'components/feedback-form';
 
 const cards = [
     //{ text: 'Hello', linkText: 'someLink', href: '/' }
 ];
+
+const explainer = `
+In order to improve lesson results, we'd like to hear from you!`;
 
 export default async function Page() {
     return (
