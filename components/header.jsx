6b9258a884ee87428c9b647ea0ac6813054879bc
tbakken14 <tbakken14@gmail.com>
2025-03-11 01:19:55 -0700

    Remove link

--- components/header.jsx
+++ components/header.jsx
@@ -5,12 +5,9 @@ import githubLogo from 'public/images/github-mark-white.svg';
 
 export function Header() {
     return (
-        <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
-            <Link href="/">
-                <Image src={netlifyLogo} alt="Netlify logo" />
-            </Link>
+        <>
             <div className='logo-s1'></div>
             <div className='logo-s2'></div>
-        </nav>
+        </>
     );
 }
