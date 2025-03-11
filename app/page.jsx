f72135046e909cadd946adf8bb7747443b4ae339
tbakken14 <tbakken14@gmail.com>
2025-03-11 01:07:11 -0700

    Fix formating

--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,16 @@ import { Markdown } from 'components/markdown';
 import { FeedbackForm } from 'components/feedback-form';
 
 const explainer = `
-In order to improve clinics, we'd like to hear from you!`;
+In order to improve clinics, we'd like to hear from you!
+`;
 
 export default async function Page() {
     return (
         <>
             <h1 className="flex w-full justify-center">Ski Form</h1>
-            <Markdown content={explainer} className="flex w-full justify-center"/>
+            <div className="flex w-full justify-center">
+                <Markdown content={explainer}/>
+            </div>
             <div className="flex w-full pt-12 justify-center">
                 <FeedbackForm />
             </div>
